fc06c436df2f7bffe0d3d367d95e357aaa603e64
Nattapon Nimakul <totizy@gmail.com>
2018-06-26 20:03:25 +0700

    remove unsued lines

--- src/components/CrowdsaleStat.vue
+++ src/components/CrowdsaleStat.vue
@@ -1,15 +1,6 @@
 <template>
   <div class="crowdsale_stat">
 
-    <!-- <section class="section">
-    <div class="container">
-      <h1 class="title">Section</h1>
-      <h2 class="subtitle">
-        A simple container to divide your page into <strong>sections</strong>, like the one you're currently reading
-      </h2>
-    </div>
-    </section> -->
-
     <div class="hero-body">
       <div class="container">
         <h1 class="title">
